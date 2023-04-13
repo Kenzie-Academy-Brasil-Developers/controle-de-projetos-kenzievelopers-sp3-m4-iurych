5b3c5a9feff17d140a6fa867d447acffab6baa26
iurych <iury.vibe@gmail.com>
2023-04-13 15:42:27 -0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from "express";
-import "dotenv/config";
+import express, { Application } from 'express';
+import 'dotenv/config';
 
 const app: Application = express();
+app.use(express.json());
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { startDatabase } from "./database";
 
-const appPort = process.env.APP_PORT || 3000 ;
+const appPort: number = Number(process.env.APP_PORT) || 3000 ;
 
 const server = (port: number) =>
   app.listen(port, async () => {
@@ -9,7 +9,7 @@ const server = (port: number) =>
     console.log(`Server is running on port ${port}.`);
   });
 
-if (process.env.NODE_ENV === "dev") {
+if (process.env.NODE_ENV === "dev" || "test") {
   server(appPort);
 }
 
